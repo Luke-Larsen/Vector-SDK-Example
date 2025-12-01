1ed28f5a2d5ee4272705dd7eafb49bb951da870b
Luke Larsen <luke@lukehanslarsen.com>
2025-12-01 15:47:36 -0800

    Basic chat and response handling

--- Group/JoinAndListen/src/main.rs
+++ Group/JoinAndListen/src/main.rs
@@ -27,14 +27,14 @@ async fn main() -> Result<(), Box<dyn Error>>{
         let bot_clone = bot.clone();
         async move {
             if let RelayPoolNotification::Event { event, .. } = notification {
-                println!("Incoming event: {:#?} ", event);
+                // println!("Incoming event: {:#?} ", event);
                 if event.kind == Kind::GiftWrap {
                     match bot_clone.client.unwrap_gift_wrap(&event).await {
                         Ok(UnwrappedGift { rumor, sender }) => {
-                            println!("Incoming rumor: {:#?} ", rumor);
+                            // println!("Incoming rumor: {:#?} ", rumor);
 
                             if rumor.kind == Kind::PrivateDirectMessage {
-                                println!("Received message: {:?}", rumor.content.trim());
+                                // println!("Received message: {:?}", rumor.content.trim());
 
                                 // Get the chat channel for the sender
                                 let chat = bot_clone.get_chat(sender).await;
@@ -53,12 +53,11 @@ async fn main() -> Result<(), Box<dyn Error>>{
 
                                 println!("Welcome Event Nostr Id: {:#?}", rumor.id);
 
-                                // First we get our group
-                                // TODO: add in a passthrough for the group id
-                                let group = bot_clone.join_group(rumor).await;
+                                // TODO: Set up checkout_group so that a bot can validate things before joining
+                                
 
-                                // Now we accept the group welcome if we like it
-                                // let _ = group.accept_group_invite(rumor.id).await;
+                                // First we get our group
+                                let group = bot_clone.join_group(rumor).await;
 
                                 let _ = tokio::time::sleep(tokio::time::Duration::from_millis(10000)).await;
 
@@ -75,10 +74,52 @@ async fn main() -> Result<(), Box<dyn Error>>{
                         Err(e) => println!("Impossible to decrypt direct message: {e}"),
                     }
                 } else if event.kind == Kind::MlsGroupMessage {
-                    println!("We got a group message!");
+                    println!("We got a group message! {:#?}",event);
                     // Process group message here
+                    let group_message = bot_clone.process_group_message(&event).await;
 
+                    println!("{:#?}", group_message);
+                    // Check what "Kind" of message we got
+                   
+                    // if group_message.kind == Kind::ApplicationSpecificData {
+                    //     println!("ApplicationSpecificData");
+                    //     if group_message.content == "typing" {
+                    //         println!("typing data");
+                    //     }
+                    // }else{
+                    //     println!("Not filter for Kind");
+                    // }
+                    match group_message.kind{
+                        Kind::ApplicationSpecificData => {
+                            if group_message.content == "typing" {
+                                println!("typing data");
+                            }
+                        }
+                        Kind::PrivateDirectMessage => {
+                            // Handle your command system
+                            match group_message.content.trim().to_lowercase().as_str() {
+
+                                "/help" =>{
+                                    let group = bot_clone.get_group(group_message.mls_group_id).await;
+                                    // Lets send a message to our newly joined group
+                                    let message_result = group.send_group_message("I will not help you").await;
+
+                                    println!("Our message result: {:#?}", message_result);
+                                },
+                                _ =>{
+                                    let group = bot_clone.get_group(group_message.mls_group_id).await;
+                                    // Lets send a message to our newly joined group
+                                    let message_result = group.send_group_message("Not a command").await;
+
+                                    println!("Our message result: {:#?}", message_result);
+                                }
+                            }
+                        }
+                        _ =>{
+                            println!("Not filter for Kind");
+                        }
                     }
+                        
                 }
             }
             
